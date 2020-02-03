538d254fa59edc68d6a43bb6a28398002da001d7
jacksonic777 <jacksonic777@gmail.com>
2020-02-03 13:49:07 +0900

    プロジェクト ファイルを追加します。

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Timer
+{
+    partial class FormTimer
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textSetTime = new System.Windows.Forms.TextBox();
+            this.textRemainingTime = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timerControll = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "時間設定";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "秒";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "残り時間";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(211, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "秒";
+            // 
+            // textSetTime
+            // 
+            this.textSetTime.Location = new System.Drawing.Point(103, 23);
+            this.textSetTime.Name = "textSetTime";
+            this.textSetTime.Size = new System.Drawing.Size(102, 19);
+            this.textSetTime.TabIndex = 4;
+            this.textSetTime.Text = "10";
+            this.textSetTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textRemainingTime
+            // 
+            this.textRemainingTime.Location = new System.Drawing.Point(103, 95);
+            this.textRemainingTime.Name = "textRemainingTime";
+            this.textRemainingTime.Size = new System.Drawing.Size(102, 19);
+            this.textRemainingTime.TabIndex = 4;
+            this.textRemainingTime.Text = "10";
+            this.textRemainingTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(40, 53);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(188, 31);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "スタート！";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timerControll
+            // 
+            this.timerControll.Interval = 1000;
+            // 
+            // FormTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 155);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textRemainingTime);
+            this.Controls.Add(this.textSetTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormTimer";
+            this.Text = "タイマー";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textSetTime;
+        private System.Windows.Forms.TextBox textRemainingTime;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timerControll;
+    }
+}
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class FormTimer : Form
+    {
+
+        int endTime; // 終了時間
+        int nowTime; // 経過時間
+        int remainingTime; // 残り時間
+
+        public FormTimer()
+        {
+            InitializeComponent();
+        }
+
+        // ボタンクリック時の処理
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // 時間設定のTextBoxの内容を終了時間の変数に取得
+            // 残り時間を計算するため経過時間の変数を0で初期化
+            // タイマースタート
+
+        }
+    }
+}
